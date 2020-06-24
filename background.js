ec00177c8ca6de4d661a059dc261449a03f418aa
Edwin A <edwin@theroyalstudent.com>
2020-06-25 01:37:23 +0800

    Enable this extension for all *.carousell.com hosts

--- background.js
+++ background.js
@@ -4,8 +4,9 @@ chrome.runtime.onInstalled.addListener(function() {
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
 		chrome.declarativeContent.onPageChanged.addRules([{
 			conditions: [
+				// this will ensure that the extension page will show up for all listing pages on *.carousell.com/p/*
 				new chrome.declarativeContent.PageStateMatcher({
-					pageUrl: { hostEquals: 'sg.carousell.com', pathPrefix: '/p/' },
+					pageUrl: { hostSuffix: '.carousell.com', pathPrefix: '/p/' },
 				})
 			],
 
